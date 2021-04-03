936bed96a7a5d26928dee38e1ffcd8236c63d810
Manuel Duarte <carlosd99m@gmail.com>
2021-04-02 18:11:53 -0600

    Area del triangulo

---
+++ src/ejercicio1.kt
@@ -0,0 +1,16 @@
+/*
+    Hallar el área de un triangulo
+
+    area = (5 * 6) / 2 = 15
+
+ */
+
+fun main (args: Array<String>) {
+
+    var base = 5;
+    var altura = 6;
+    var area = base * altura / 2
+
+    println("El área del triángulo es igual a $area")
+
+}
