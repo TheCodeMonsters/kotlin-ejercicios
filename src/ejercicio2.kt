8d724153f780b866bc49e8dcacb34636f0725a74
Manuel Duarte <carlosd99m@gmail.com>
2021-04-02 18:28:06 -0600

    Hallar el área de un círculo :art:

---
+++ src/ejercicio2.kt
@@ -0,0 +1,10 @@
+/**
+ * Crear un programa para calcular el área de un circulo
+ */
+
+fun main (args: Array<String>) {
+    val radio = 5
+    val area = "${radio * radio} π "
+
+    println("El valor del área del circulo es igual a $area")
+}
